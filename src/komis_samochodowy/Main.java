4c63d4c07a1627296b149120ac2aeffdd639ead3
Magdalena Zawodny <magdalenazawodny98@gmail.com>
2021-06-18 13:55:57 +0200

    remove comments

--- src/komis_samochodowy/Main.java
+++ src/komis_samochodowy/Main.java
@@ -3,28 +3,12 @@ package komis_samochodowy;
 import java.awt.EventQueue;
 
 public class Main {
-	public static void main(String[] args){
-//	    Car myCar = new Car(1, "Ford", "czarny", "Focus", 15000.0, "Combi");
-//	   
-//	   String myCarDescription = myCar.toString();
-//	    
-//	   System.out.println(myCarDescription);
-	   
+	public static void main(String[] args){	   
 		EventQueue.invokeLater(new Runnable() {
  			@Override
  			public void run() {
  				new MyFrame();
  			}
- 		});
-	     
-//	    
-//	    myCar.setID(100);
-//	    
-//	    System.out.println("A teraz moj samochod ma ID: " + myCar.getID());
-//
-//	    System.out.println("A teraz moj samochod ma kolor: " + myCar.getColor());	    
-	    
-	  }
-	
-	
+ 		});	    
+	  }	
 }
